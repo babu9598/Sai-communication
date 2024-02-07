163814df2b86b86fadd4ef77606f49fc400fa217
babu9598 <148607675+babu9598@users.noreply.github.com>
2024-02-07 18:19:03 +0530

    Add files via upload

---
+++ APPLE iPhone 14 (Blue, 128 GB).php
@@ -0,0 +1,329 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+    <link rel="icon" href="assets/images/picon.jpg">
+    <title>Sai Communication</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+  </head>
+
+  <body>
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+
+    <!-- Header -->
+    <div class="sub-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-xs-12">
+            <ul class="left-info">
+              <li><a href="#"><i class="fa fa-envelope"></i>saicommunication@gmail.com</a></li>
+              <li><a href="#"><i class="fa fa-phone"></i>+917010911854</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4">
+            <ul class="right-icons">
+              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <header class="">
+      <nav class="navbar navbar-expand-lg">
+        <div class="container">
+          <a class="navbar-brand" href="index.html"><h2>Sai Communication</h2></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.html">Home
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="products.html">Products</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="checkout.html">Checkout</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="dropdown-toggle nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">About</a>
+              
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="about.html">About Us</a>
+                    <a class="dropdown-item" href="testimonials.html">Testimonials</a>
+                    <a class="dropdown-item" href="terms.html">Terms</a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.html">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <!-- Page Content -->
+    <div class="page-heading header-text">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h1><small><del><sup></sup> </del></small> &nbsp; <sup></sup></h1>
+            <span>
+                
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-7">
+            <div>
+              <img src="assets/images/apple.jpg" alt="" class="img-fluid wc-image">
+            </div>
+
+            <div class="row">
+              <div class="col-sm-4 col-6">
+                <div>
+                  <img src="assets/images/apple1.jpg" alt="" class="img-fluid">
+                </div>
+                <br>
+              </div>
+              <div class="col-sm-4 col-6">
+                <div>
+                  <img src="assets/images/apple2.jpg" alt="" class="img-fluid">
+                </div>
+                <br>
+              </div>
+              <div class="col-sm-4 col-6">
+                <div>
+                  <img src="assets/images/apple3.jpg" alt="" class="img-fluid">
+                </div>
+                <br>
+              </div>
+            </div>
+
+            <br>
+          </div>
+
+          <div class="col-md-5">
+            <div class="sidebar-item recent-posts">
+              <div class="sidebar-heading">
+                <br>
+                <br>
+                <br>
+                <br>
+                <h4>APPLE iPhone 14 (Black, 128 GB)</h4>
+              </div>
+
+              <div class="content">
+                128 GB ROM <br>
+                15.49 cm (6.1 inch) Super Retina XDR Display<br>
+                12MP + 12MP | 12MP Front Camera<br>
+                A15 Bionic Chip, 6 Core Processor Processor<br>
+              </div>
+            </div>
+
+            <br>
+            <br>
+          
+            <h4>Add to Cart</h4>
+
+            <br>
+
+            <form action="" method="post">
+             
+                
+            
+
+              <div class="row">
+                <div class="col-md-6 col-sm-12">
+                  <div class="form-group">
+                    <label for="">Quantity</label>
+                    <input type="text" value="1" required="" class="form-control">
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <div class="form-group">
+                    <a href="checkout.php"class="filled-button">Add to Cart</a>
+                  </div>
+                </div>
+              </div>
+            </form>
+
+            <br>
+          </div>
+        </div>
+
+        <br>
+        </br>
+       
+<br>
+
+      <b>Description</b> 
+      Stay productive and improve your performance with the Super Retina XDR display that is comfortable for the eyes. Powered with a 12 MP main camera, enjoy taking pictures with friends and family. With a built-in rechargeable lithium-ion battery and equipped with the MagSafe wireless charging, you can charge your phone quickly up to 50 % in just half an hour by using a 20 W adapter. This phone is loaded with a horde of exciting features such as Siri, face ID, barometer, ambient light sensors etc., and is also resistant to dust and water as it is IP68 rated.<br>
+
+        <b>Super Retina XDR Display</b>
+        The iPhone 14 display has rounded corners that follow a beautiful curved design, and these corners are within a standard rectangle. When measured as a standard rectangular shape, the screen is 15.40 centimetres (6.06) diagonally.</p>
+
+        <br>
+        <b>Specifications</b>
+        <h6>General</h6>
+        In The Box
+        Handset, USB-C to Lightning Cable, Documentation<br>
+        Model Number    : MPVN3HN/A<br>
+        Model Name      : iPhone 14<br>
+        Color           : Black<br>
+        Browse Type     : Smartphones<br>
+        SIM Type        : Dual Sim(Nano + eSIM)<br>
+        Hybrid Sim Slot : No<br>
+        Touchscreen     : Yes<br>
+        OTG Compatible  : No<br>
+        Quick Charging  : Built-in Stereo Speaker<br>
+        <br>
+        Display Features<br>
+        Display Size           : 15.49 cm (6.1 inch)<br>
+        Resolution             : 2532 x 1170 Pixels<br>
+        Resolution Type        : Super Retina XDR Display<br>
+        GPU                    : 5 Core<br> 
+        Display Type           : Super Retina XDR Display<br>
+        Other Display Features : HDR Display, True Tone, Wide Colour (P3), Haptic Touch, Contrast Ratio: 20,00,000:1, Max<br>
+                                 Brightness: 800 nits, Peak Brightness: 1,200 nits, Fingerprint Resistant Oleophobic Coating,<br>
+                                 Support for Display of Multiple Languages and Characters Simultaneously
+
+        <br>
+      </div>
+    </div>
+
+       <!-- Footer Starts Here -->
+       <footer>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-3 footer-item">
+              <h4>Sai Communication</h4>
+              <p>One of the Largest sales and service Shop in Tamilnadu, spanning across 3+ shops around Tamil Nadu famous for their touch & feel experience.</p>
+              <ul class="social-icons">
+                <li><a rel="nofollow" href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+              </ul>
+            </div>
+            <div class="col-md-3 footer-item">
+              <h4>Useful Links</h4>
+              <ul class="menu-list">
+                <li><a href="#">Whatsapp</a></li>
+                <li><a href="#">Instagram</a></li>
+                <li><a href="#">Twitter</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Address</a></li>
+              </ul>
+            </div>
+            <div class="col-md-3 footer-item">
+              <h4>Additional Pages</h4>
+              <ul class="menu-list">
+                <li><a href="#">Products</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Terms</a></li>
+              </ul>
+            </div>
+            <div class="col-md-3 footer-item last-item">
+              <h4>Contact Us</h4>
+              <div class="contact-form">
+                <form id="contact footer-contact" action="" method="post">
+                  <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                      <fieldset>
+                        <input name="name" type="text" class="form-control" id="name" placeholder="Full Name" required="">
+                      </fieldset>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                      <fieldset>
+                        <input name="email" type="text" class="form-control" id="email" pattern="[^ @]*@[^ @]*" placeholder="E-Mail Address" required="">
+                      </fieldset>
+                    </div>
+                    <div class="col-lg-12">
+                      <fieldset>
+                        <textarea name="message" rows="6" class="form-control" id="message" placeholder="Your Message" required=""></textarea>
+                      </fieldset>
+                    </div>
+                    <div class="col-lg-12">
+                      <fieldset>
+                        <button type="submit" id="form-submit" class="filled-button">Send Message</button>
+                      </fieldset>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      
+      <div class="sub-footer">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <p>
+                  Copyright © 2023 Company Name: <a href="#">Sai Communication</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+  
+      <!-- Bootstrap core JavaScript -->
+      <script src="vendor/jquery/jquery.min.js"></script>
+      <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  
+      <!-- Additional Scripts -->
+      <script src="assets/js/custom.js"></script>
+      <script src="assets/js/owl.js"></script>
+      <script src="assets/js/slick.js"></script>
+      <script src="assets/js/accordions.js"></script>
+  
+      <script language = "text/Javascript"> 
+        cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+        function clearField(t){                   //declaring the array outside of the
+        if(! cleared[t.id]){                      // function makes it static and global
+            cleared[t.id] = 1;  // you could use true and false, but that's more typing
+            t.value='';         // with more chance of typos
+            t.style.color='#fff';
+            }
+        }
+      </script>
+  
+    </body>
+  </html>
---
+++ about.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+    <link rel="icon" href="assets/images/picon.jpg">
+    <title>Sai Communication</title>
+
+    <!-- Bootstrap  -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <!--  CSS Files  -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+  </head>
+
+  <body>
+ 
+    <!-- Header -->
+    <div class="sub-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-xs-12">
+            <ul class="left-info">
+              <li><a href="#"><i class="fa fa-envelope"></i>saicommunication@gmail.com</a></li>
+              <li><a href="#"><i class="fa fa-phone"></i>+917010911854</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4">
+            <ul class="right-icons">
+              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <header class="">
+      <nav class="navbar navbar-expand-lg">
+        <div class="container">
+          <a class="navbar-brand" href="index.php"><h2>Sai Communication</h2></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="about.php">Home
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="products.php">Products</a>
+              </li>
+            
+              <li class="nav-item dropdown active">
+                <a class="dropdown-toggle nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">About</a>
+              
+                <div class="dropdown-menu">
+                    <a class="dropdown-item active" href="about.php">About Us</a>
+
+                    <a class="dropdown-item " href="testimonials.php">Testimonials</a>
+                    <a class="dropdown-item" href="terms.php">Terms</a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="login.php">Login</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+
+    <!-- Page Content -->
+    <div class="page-heading header-text">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h1>About Us</h1>
+            <span>We have over 20 years of experience</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="more-info about-info">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="more-info-content">
+              <div class="row">
+                <div class="col-md-6 align-self-center">
+                  <div class="right-content">
+                    <span>Sai Communication</span>
+                    <h2>Get to know about <em>our shop</em></h2>
+                    <p>Sai Communication is one of the Largest Sale and Service shop in Tamilnadu  spanning across 3+ shops  Tamil Nadu famous for their touch & feel experience. 
+                    We sells a wide category of devices in its showrooms and Online portal, ranging from the Best Smartphones, Laptops, Computers, Smart Devices, Smart TVs to Accessories.
+                     </p>
+                     <script src="assets/js/custom.js"></script> <script src="assets/js/owl.js"></script><script src="assets/js/slick.js"></script>
+                     <script src="assets/js/accordions.js"></script>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="left-image">
+                    <img src="assets/images/SaiBanner.jpg" alt="">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="fun-facts">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="left-content">
+              <span></span>
+              <h2>Sai Communication <em>Mobile shop</em></h2>
+              <p>Sai Communication's  E-Commerce platform www.saicommunication.com caters to customers across India where Customers can Comfortably Order their devices with just a tap and get them delivered Safely with delivery options such as  Same Day Delivery, Next Day Delivery, and a Pickup at the Store option based on their location. 
+                Having served over 100k+ Happy Customers, Poorvika takes pride in being India's leading retailer for Top Brands like Apple, Samsung, Oppo, Vivo, Xiaomi, OnePlus, Redmi, Realme, Nokia, etc.</p>
+            </div>
+          </div>
+          <div class="col-md-6 align-self-center">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="count-area-content">
+                  <div class="count-digit">2000+</div>
+                  <div class="count-title">Products</div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="count-area-content">
+                  <div class="count-digit">100k+</div>
+                  <div class="count-title">Happy clients</div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="count-area-content">
+                  <div class="count-digit">1</div>
+                  <div class="count-title">City</div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="count-area-content">
+                  <div class="count-digit">3</div>
+                  <div class="count-title">Stores</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+         <!-- Footer Starts Here -->
+         <?php include 'footer.php' ;?>
+
+
+        <!-- Bootstrap -->
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+     
+    
+      </body>
+        </html>
